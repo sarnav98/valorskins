0993f8be9379f7626d1ba234b477d91b7849bb78
thulka2 <47118848+thulka2@users.noreply.github.com>
2020-09-24 14:32:50 -0500

    Updates

---
+++ precache-manifest.cd12990ff777662e037282fa4261a08d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd785f8cf9710fdddfa3b0ff305efe07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72e582bbb91c0fbfca21",
+    "url": "/static/css/2.f76e3c0a.chunk.css"
+  },
+  {
+    "revision": "b92607605b0bf1f6a02f",
+    "url": "/static/css/main.11d03e2f.chunk.css"
+  },
+  {
+    "revision": "72e582bbb91c0fbfca21",
+    "url": "/static/js/2.159530dd.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.159530dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b92607605b0bf1f6a02f",
+    "url": "/static/js/main.a35d3d6c.chunk.js"
+  },
+  {
+    "revision": "93217ac5235a342778d5",
+    "url": "/static/js/runtime-main.b2114d5a.js"
+  },
+  {
+    "revision": "73445b4ed7b0d353331bf6fd5077560f",
+    "url": "/static/media/192icon.73445b4e.png"
+  },
+  {
+    "revision": "4ddfa97224424304261aa4cd270cdf4f",
+    "url": "/static/media/Ares.4ddfa972.svg"
+  },
+  {
+    "revision": "fa6187a4a5cadcc85ed1bd64d8101687",
+    "url": "/static/media/Bucky.fa6187a4.svg"
+  },
+  {
+    "revision": "5871fe8d87f0b6ca56274affc013264e",
+    "url": "/static/media/Bulldog.5871fe8d.svg"
+  },
+  {
+    "revision": "cb46a0040ff5735e48828b99b029cbd2",
+    "url": "/static/media/Classic.cb46a004.svg"
+  },
+  {
+    "revision": "caff862d1ecf9195d6ae20ac3d859d6f",
+    "url": "/static/media/Frenzy.caff862d.svg"
+  },
+  {
+    "revision": "143657279571eb05caad76a1e2ebf8fb",
+    "url": "/static/media/Ghost.14365727.svg"
+  },
+  {
+    "revision": "c6cbee6adce0a607c3c1266b4469933e",
+    "url": "/static/media/Guardian.c6cbee6a.svg"
+  },
+  {
+    "revision": "88695a409473f331d1e380442eca8cb6",
+    "url": "/static/media/Judge.88695a40.svg"
+  },
+  {
+    "revision": "1845527164a520344d12d18c77c4d850",
+    "url": "/static/media/Knife.18455271.svg"
+  },
+  {
+    "revision": "ef93c0d59d4a1cc29fc5b96b3bf318ed",
+    "url": "/static/media/Marshal.ef93c0d5.svg"
+  },
+  {
+    "revision": "d62ad3ba638df219c8a82ed033e18866",
+    "url": "/static/media/Odin.d62ad3ba.svg"
+  },
+  {
+    "revision": "9dcee191ce1840b9090412d71ef73392",
+    "url": "/static/media/Operator.9dcee191.svg"
+  },
+  {
+    "revision": "0fabead9d81f1539c13347c45fbf81dc",
+    "url": "/static/media/Phantom.0fabead9.svg"
+  },
+  {
+    "revision": "4f7828e54a0575bfd8d37595336be121",
+    "url": "/static/media/Sheriff.4f7828e5.svg"
+  },
+  {
+    "revision": "e351fd53183c811baad8c5946b6be86e",
+    "url": "/static/media/Shorty.e351fd53.svg"
+  },
+  {
+    "revision": "db80d6912326f1e67a037bb0cb445bb3",
+    "url": "/static/media/Spectre.db80d691.svg"
+  },
+  {
+    "revision": "fe6ed1017e37cf8d28d947ad17f0b8db",
+    "url": "/static/media/Stinger.fe6ed101.svg"
+  },
+  {
+    "revision": "d416bf8fd8070ddc8e01496b0ca7391c",
+    "url": "/static/media/Vandal.d416bf8f.svg"
+  }
+]);
