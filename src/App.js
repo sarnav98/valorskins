57c7662fe85ca033669f4da8b66d42e2447fdae3
thulka2 <47118848+thulka2@users.noreply.github.com>
2020-10-01 15:13:11 -0500

    GA test 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import './App.css';
 
 import {Layout, Header, HeaderRow, HeaderTabs, Tab, Drawer, Content, Footer, FooterSection, FooterLinkList} from 'react-mdl';
